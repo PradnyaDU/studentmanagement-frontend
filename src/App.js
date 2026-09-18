5b51701b243cfcc63b4e35a7a9f40f3ec2b139f1
Pradnya <deshpandepradnya18@gmail.com>
2026-09-19 01:58:26 +0530

    sent objects in state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import GetAllStudents from "./Components/GetAllStudents";
 import "./Components/Hello";
+import { useState } from "react";
 import Hello from "./Components/Hello";
 export default function App() {
   const myname = ["Harshal", "Pradnya", "Rutuja", "Sanket"];
+
+  const [name, setName] = useState("");
+  const [student, setStudent] = useState({
+    stuname: "Pradnya",
+    stuage: 23,
+    stucity: "Pune",
+  });
+
   function HelloMessage() {
     return "Hello This is HelloMessage function";
+  }
+
+  function changeStudent(name, age, city) {
+    setStudent({ stuname: name, stuage: age, stucity: city });
   }
   return (
     <>
       {/* {myname} */}
       {/* <GetAllStudents className="form-control" /> */}
-      <Hello names={myname} age={25} city="New York" />
-      <Hello label="This is my label" func={HelloMessage} />
+      <Hello
+        names={myname}
+        age={25}
+        city="New York"
+        label="This is my label"
+        func={HelloMessage}
+      />
+      <br />
+      <button
+        onClick={() => setName(name === "Pradnya" ? "Harshal" : "Pradnya")}
+      >
+        Click me 😉
+      </button>{" "}
+      <br />
+      <input type="text" onChange={(e) => setName(e.target.value)}></input>
+      <p> name is : {name}</p>
+      <button onClick={() => changeStudent("Harshal", 24, "Mumbai")}>
+        Change Student
+      </button>
+      <p>
+        Name: {student.stuname}
+        <br />
+        Age: {student.stuage}
+        <br />
+        City: {student.stucity}
+      </p>
     </>
   );
 }
--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -7,25 +7,22 @@ export default function Hello({
   label,
   func,
 }) {
-  const [name, SetName] = useState("Pradnya");
-  function handleClick() {
-    SetName("Harshal");
-  }
+  //   function handleClick() {
+  //     SetName("Harshal");
+  //   }
   return (
     <>
-      <h1>
-        Hello, {names}! You are {age} years old and live in {city}.
-      </h1>
       {/* <h1>
+        Hello, {names}! You are {age} years old and live in {city}.
+      </h1> */}
+      <h1>
         {names.map((name, index) => (
           <li key={index}>{name}</li>
         ))}
-      </h1> */}
+      </h1>{" "}
       <div>
-        <button onClick={() => alert(func())}>{label}</button>
+        {func && <button onClick={() => alert(func())}>{label}</button>}
         <br></br>
-        <button onClick={handleClick}>Click me</button>
-        <p>Count is : {name}</p>
       </div>
     </>
   );
