5099af8484ae2693a295a599610408e91e3cbaf5
Pradnya <deshpandepradnya18@gmail.com>
2026-09-22 21:08:39 +0530

    add routing with react-router-dom and create About and User components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import CreateStudent from "./Components/CreateStudent";
 import DeleteStudent from "./Components/DeleteStudent";
+import About from "./Practice/About";
+import User from "./Practice/User";
+
 import FetchAPI from "./Practice/FetchAPI";
 import UseEffectHandsOn from "./Practice/UseEffectHandsOn";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 export default function App() {
-  return <FetchAPI></FetchAPI>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">About</Link>
+        <Link to="/Users/10">Users</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<About></About>} />
+        <Route path="/Users/:id" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/Practice/User.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+export default function User() {
+  const { id } = useParams();
+  return <div>User page: {id}</div>;
+}
